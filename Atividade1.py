f6af347d86b8f84e23b01ed0cc22daf05ea805d6
Yasmin Lopes <yasminlopes@alu.ufc.br>
2026-07-17 16:11:49 -0300

    Create Atividade1.py

---
+++ Atividade1.py
@@ -0,0 +1,10 @@
+var1 = "Eu sei fazer print assim "
+var2 = "e assim: "
+print(var1 + "Hello World")
+print (var2 + "Hello World")
+somavar1 = "25"
+somavar2= "7"
+print("A soma dos caracteres da var1 é "+somavar1 + " e var2 é: " +somavar2)
+resto = 25%7
+print("Dividindo o valor da 1º variável pela 2º variável, obtém-se o resto de: ")
+print (resto)
