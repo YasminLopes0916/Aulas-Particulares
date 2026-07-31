b3ca78f6ef5ec8d760d0311e0ad4a12365c5768a
Yasmin Lopes <yasminlopes@alu.ufc.br>
2026-07-31 16:33:16 -0300

    Atividadesrevisao.py

--- Atividadesrevisao.py
+++ Atividadesrevisao.py
@@ -1,1 +1,14 @@
+#parte1
+minha_idade = 25
+sobre_idade = "Eu tenho " + str(minha_idade) + " anos"
+#print (sobre_idade)
+#parte2
+minha_altura = 1.63 
+minha_idade += minha_altura
+print("e minha altura " + str(minha_altura) + " somada a minha idade resulta em " + str(minha_idade))
+#parte3
+teste_altura = True
+teste_idade = False
+#print("e minha altura " + str(minha_altura) + " é " + str(teste_altura) + " e minha idade " + str(minha_idade) + " é " + str(teste_idade))
+
 
