2e247b5e1c0fe14d6f54c18f94e438aaeb12b621
Yasmin Lopes <yasminlopes@alu.ufc.br>
2026-07-31 16:56:41 -0300

    Atividade2if.py

---
+++ Atividade2if.py
@@ -0,0 +1,8 @@
+protagonista = 11
+antagonista = 11
+if protagonista > antagonista:
+  print("O protagonista venceu a batalha e sobraram flechas!")
+if protagonista < antagonista:
+  print("O antagonista venceu a batalha!")
+if protagonista == antagonista:
+  print("O protagonista venceu a batalha usando todas as flechas!")
